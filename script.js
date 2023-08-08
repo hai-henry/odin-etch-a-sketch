8647087ef6928b41ec18a83557b9ab1fcaccba77
hai-henry <henry.h.ho-1@ou.edu>
2023-08-08 10:18:46 -0500

    Add function to resize grid

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const container = document.querySelector(".grids");
+const container = document.querySelector(".grid-container");
+const grids = document.getElementById("grids");
 
-for (let row = 0; row < 16; ++row) {
-	for (let column = 0; column < 16; ++column) {
-		const cell = document.createElement("div");
-		cell.classList.add("cell");
-		container.appendChild(cell);
+setGrid(20);
+
+function setGrid(size) {
+	grids.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	for (let row = 0; row < size; ++row) {
+		for (let column = 0; column < size; ++column) {
+			const cell = document.createElement("div");
+			cell.classList.add("cell");
+			container.appendChild(cell);
+		}
 	}
 }
 
-const gridSelection = document.querySelector(".grids");
-gridSelection.addEventListener("mouseover", () => {
+container.addEventListener("mouseover", () => {
 	draw();
 });
 
