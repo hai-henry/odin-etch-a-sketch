d0af799a8f5149b580f9950c69a0676e78860ae3
hai-henry <henry.h.ho-1@ou.edu>
2023-08-09 12:31:15 -0500

    Add size slider

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const container = document.querySelector(".grid-container");
 const grids = document.getElementById("grids");
+const size = document.getElementById("slider");
 
 setGrid(16);
-draw();
+
+size.addEventListener("input", function () {
+	clearGrid();
+	const updateSize = parseInt(size.value);
+	setGrid(updateSize);
+});
+
+function clearGrid() {
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+}
 
 function setGrid(size) {
 	grids.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-	grids.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+	grids.style.gridTemplateRows = `repeat(${size},  1fr)`;
 	for (let i = 0; i < size ** 2; ++i) {
 		const cell = document.createElement("div");
 		cell.classList.add("cell");
 		cell.setAttribute("draggable", false);
+		draw();
 		container.appendChild(cell);
 	}
 }
